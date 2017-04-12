6535c2d34b98e2567ba720b9dd54506037e334a8
Gaspard SCHAETZ <gschaetz@e2r5p10.42.fr>
2017-04-12 17:11:12 +0200

    fixe modif 3

--- ft_printf/ft_appli.c
+++ ft_printf/ft_appli.c
@@ -6,7 +6,7 @@
 /*   By: gschaetz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 07:44:05 by gschaetz          #+#    #+#             */
-/*   Updated: 2017/04/12 17:04:43 by gschaetz         ###   ########.fr       */
+/*   Updated: 2017/04/12 17:10:17 by gschaetz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,7 +105,13 @@ char		*ft_zero(t_printf *var, char *tmp, char *cp)
 	if (var->format_split[var->ich].flag[0] == ' ')
 		tmp[var->ct++] = ' ';
 	while (var->ct < max)
-		tmp[var->ct++] = '0';
+	{
+		if (var->format_split[var->ich].prec <= var->lenf && \
+				ft_strchr(var->format_split[var->ich].stock, '.') != NULL)
+			tmp[var->ct++] = ' ';
+		else
+			tmp[var->ct++] = '0';
+	}
 	return (tmp);
 }
 
